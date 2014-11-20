c8cdabec5c111d5a83ea38df2d100eea0187034c
yweistra <yweistra@yde-weistras-iMac-4.local>
2014-11-20 23:21:52 +0100

    testing

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         NSURLCache.setSharedURLCache(cache)
        
         
+        //test github
+        
         FBLoginView.self
         FBProfilePictureView.self
         
